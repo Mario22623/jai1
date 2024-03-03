60d0628ec0606e31b51255b36a12b46bec0e868e
JAIDEEP <110852245+Mario22623@users.noreply.github.com>
2024-03-03 22:46:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-const videoPlayer = document.getElementById('videoPlayer');
-const audioPlayer = document.getElementById('audioPlayer');
+const videoElement = document.getElementById('video');
+const audioTracksSelect = document.getElementById('audio-tracks');
+const switchAudioButton = document.getElementById('switch-audio');
 
-// Play audio
-audioPlayer.play();
+// Fetch and populate audio tracks
+videoElement.addEventListener('loadedmetadata', () => {
+    const audioTracks = videoElement.audioTracks;
 
-// Play video when audio is loaded
-audioPlayer.oncanplay = function() {
-    videoPlayer.play();
-};
+    for (let i = 0; i < audioTracks.length; i++) {
+        const track = audioTracks[i];
+        const option = document.createElement('option');
+        option.value = i; 
+        option.text = `Track ${i + 1} (${track.label})`;
+        audioTracksSelect.appendChild(option);
+    }
+});
+
+// Switch audio track
+switchAudioButton.addEventListener('click', () => {
+    const selectedIndex = audioTracksSelect.selectedIndex;
+    if (selectedIndex >= 0) {
+        videoElement.audioTracks[selectedIndex].enabled = true;
+    }
+});
